15849caf2e347a8e698c9ff0850ea60dbe292b65
Valentina Cárdenas <valencardenas.12@hotmail.com>
2017-02-20 15:21:11 -0500

    intendo navigationdrawer

---
+++ app/src/main/java/sjmm/manualdebar/SplashActivity.java
@@ -0,0 +1,32 @@
+package sjmm.manualdebar;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final long SPLASH_DELAY=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i =new Intent().setClass(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_DELAY);
+    }
+}
